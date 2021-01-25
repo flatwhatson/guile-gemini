e84dab6b2f634503b91c83870182885bc33bf21e
Andrew Whatson <whatson@gmail.com>
2021-01-25 14:13:06 +1000

    Add support for client certificates

--- example/gem-fetch.scm
+++ example/gem-fetch.scm
@@ -4,8 +4,10 @@
 (use-modules (gemini client)
              (gemini request)
              (gemini response)
+             (gnutls)
              (ice-9 getopt-long)
              (rnrs bytevectors)
+             (srfi srfi-11)
              (web uri))
 
 (define (print-help args)
@@ -13,29 +15,49 @@
 usage: " (car args) " [options] URI
 
 options:
-  -h, --help       Display this help
-  -a, --addr       Host to connect to (default from URI)
-  -p, --port       Port to connect to (default from URI)
+  -h, --help                   Display this help
+  -p, --proxy=HOST:PORT        Proxy via HOST:PORT
+  -c, --cert=path/to/cert.pem  Client certificate file
+  -k, --pkey=path/to/pkey.pem  Client private key file
 
 Send a Gemini request and print the response.
 ")))
 
+(define (parse-proxy proxy)
+  (cond ((not proxy)
+         (values #f #f))
+        ((string-contains proxy ":")
+         (apply values (string-split proxy #\:)))
+        (else
+         (values proxy #f))))
+
+(define (load-credentials cert pkey)
+  (let ((cred (make-certificate-credentials)))
+    (when (and cert pkey)
+      (set-certificate-credentials-x509-key-files!
+       cred cert pkey x509-certificate-format/pem))
+    cred))
+
 (define (main args)
-  (let* ((option-spec '((help (single-char #\h) (value #f))
-                        (addr (single-char #\a) (value #t))
-                        (port (single-char #\p) (value #t))))
-         (opts (getopt-long args option-spec))
-         (help (option-ref opts 'help #f))
-         (addr (option-ref opts 'addr #f))
-         (port (option-ref opts 'port #f))
-         (rest (option-ref opts '() '()))
-         (uri (and (pair? rest) (car rest))))
+  (let* ((option-spec '((help  (single-char #\h) (value #f))
+                        (proxy (single-char #\p) (value #t))
+                        (cert  (single-char #\c) (value #t))
+                        (pkey  (single-char #\k) (value #t))))
+         (opts  (getopt-long args option-spec))
+         (help  (option-ref opts 'help #f))
+         (proxy (option-ref opts 'proxy #f))
+         (cert  (option-ref opts 'cert #f))
+         (pkey  (option-ref opts 'pkey #f))
+         (rest  (option-ref opts '() '()))
+         (uri   (and (pair? rest) (car rest))))
     (if (or help (not uri))
         (print-help args)
-        (let ((req (build-gemini-request #:uri (string->uri uri))))
+        (let-values (((req) (build-gemini-request #:uri (string->uri uri)))
+                     ((host port) (parse-proxy proxy))
+                     ((cred) (load-credentials cert pkey)))
           (format #t "Request: ~a\n"
                   (uri->string (gemini-request-uri req)))
-          (let ((rsp (send-gemini-request req addr port)))
+          (let ((rsp (send-gemini-request req host port cred)))
             (format #t "Response: ~a ~a\n"
                     (gemini-response-status rsp)
                     (gemini-response-meta rsp))
--- gemini/client.scm
+++ gemini/client.scm
@@ -6,6 +6,11 @@
   #:use-module (web uri)
   #:export (send-gemini-request))
 
+;; (set-log-level! 10)
+;; (set-log-procedure!
+;;  (lambda (level str)
+;;    (format #t "gnutls: [~a] ~a" level str)))
+
 (define (resolve-addresses host port)
   (delete-duplicates
    (getaddrinfo host (if port (number->string port) "1965") AI_NUMERICSERV)
@@ -27,36 +32,47 @@
               (apply throw args)
               (loop (cdr addresses))))))))
 
-(define (open-session host socket)
+(define (open-session socket host cred)
   (let ((session (make-session connection-end/client)))
     (set-session-server-name! session server-name-type/dns host)
     (set-session-transport-fd! session (fileno socket))
     (set-session-default-priority! session)
-    (set-session-credentials! session (make-certificate-credentials))
+    (set-session-credentials! session cred)
 
-    (catch 'gnutls-error
-      (lambda ()
-        (handshake session))
-      (lambda (key err proc . rest)
-        (cond ((eq? err error/warning-alert-received)
-               (format (current-error-port)
-                       "warning: TLS warning alert received: ~a~%"
-                       (alert-description->string (alert-get session)))
-               (handshake session))
-              (else
-               (apply throw key err proc rest)))))
+    (handshake session)
+
+    (let* ((data (car (session-peer-certificate-chain session)))
+           (cert (import-x509-certificate data x509-certificate-format/der)))
+      (unless (x509-certificate-matches-hostname? cert host)
+        (throw 'tls-certificate-error
+               'host-mismatch host (x509-certificate-dn cert))))
+
+    (let* ((status (peer-certificate-status session))
+           (invalid (lset-intersection
+                     eq? status (list certificate-status/expired
+                                      certificate-status/not-activated))))
+      (unless (null? invalid)
+        (throw 'tls-certificate-error
+               'invalid-certificate host invalid)))
+
+    ;; TODO: server certificate fingerprint verification
+    ;; https://drewdevault.com/2020/09/21/Gemini-TOFU.html
 
     session))
 
-(define* (send-gemini-request req #:optional host port)
+(define* (send-gemini-request req #:optional host port cred)
   (let* ((host (or host (uri-host (gemini-request-uri req))))
          (port (or port (uri-port (gemini-request-uri req))))
-         (socket (open-socket host port))
-         (session (open-session host socket))
-         (record (session-record-port session)))
+         (cred (or cred (make-certificate-credentials)))
+
+         (socket  (open-socket host port))
+         (session (open-session socket host cred))
+         (record  (session-record-port session)))
+
     (setvbuf record 'block)
     (write-gemini-request req record)
     (force-output record)
+
     (let ((rsp (read-gemini-response record)))
       (close-port record)
       (close-port socket)
