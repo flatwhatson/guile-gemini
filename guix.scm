12bde2f4f606e88f70dfa9750951b1d40de9b86c
Andrew Whatson <whatson@gmail.com>
2021-05-27 10:37:12 +1000

    Add a guix.scm

---
+++ guix.scm
@@ -0,0 +1,39 @@
+(use-modules (guix packages)
+             (guix build-system guile)
+             (guix gexp)
+             (guix git)
+             (guix git-download)
+             ((guix licenses) #:prefix license:)
+             (gnu packages guile)
+             (gnu packages guile-xyz)
+             (gnu packages tls)
+             (ice-9 popen)
+             (ice-9 textual-ports)
+             (srfi srfi-1))
+
+(define %source-dir
+  (dirname (current-filename)))
+
+(define %git-commit
+  (get-line (open-input-pipe "git rev-parse HEAD")))
+
+(define guile-gemini
+  (package
+    (name "guile-gemini")
+    (version (git-version "0.1" "HEAD" %git-commit))
+    (source (local-file %source-dir
+                        #:recursive? #t
+                        #:select? (git-predicate %source-dir)))
+    (build-system guile-build-system)
+    (inputs `(("guile" ,guile-3.0-latest)
+              ("gnutls" ,gnutls)
+              ("guile-fibers" ,guile-fibers)))
+    (home-page "https://github.com/flatwhatson/guile-gemini")
+    (synopsis "Guile implementation of the Gemini protocol")
+    (description
+     "Guile Gemini is an implementation of the Gemini protocol in Guile Scheme,
+providing both client and server functionality.  It uses GnuTLS to meet
+Gemini's TLS requirements, and Guile Fibers for concurrency.")
+    (license license:lgpl3+)))
+
+guile-gemini
