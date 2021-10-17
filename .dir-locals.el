e048bb1dd41fb20be1ff2d3ddc1615e794210d13
Gerry Agbobada <10496163+gagbo@users.noreply.github.com>
2021-10-17 21:28:24 +0200

    Add GUILE_LOAD_PATH from direnv to geiser-guile-load-path

--- .dir-locals.el
+++ .dir-locals.el
@@ -6,7 +6,9 @@
   (eval . (with-eval-after-load 'geiser-guile
             (let ((root-dir
                    (file-name-directory
-                    (locate-dominating-file default-directory ".dir-locals.el"))))
+                    (locate-dominating-file default-directory ".dir-locals.el")))
+                  (env-paths (split-string (getenv "GUILE_LOAD_PATH") ":")))
+              (when env-paths (setq-local geiser-guile-load-path env-paths))
               (unless (member root-dir geiser-guile-load-path)
                 (setq-local geiser-guile-load-path
                             (cons root-dir geiser-guile-load-path)))))))
