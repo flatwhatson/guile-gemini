727a6381b8d3174693b1c24412005448d68c5b9b
Andrew Whatson <whatson@gmail.com>
2023-04-20 09:53:15 +1000

    Propagate runtime guile dependencies

--- guix.scm
+++ guix.scm
@@ -28,9 +28,8 @@
                         #:select? (git-predicate %source-dir)))
     (build-system guile-build-system)
     (arguments '(#:source-directory "src"))
-    (inputs (list guile-3.0-latest
-                  gnutls
-                  guile-fibers-1.1))
+    (inputs (list guile-3.0-latest))
+    (propagated-inputs (list guile-fibers-1.1 guile-gnutls))
     (home-page "https://notabug.org/flatwhatson/guile-gemini")
     (synopsis "Guile implementation of the Gemini protocol")
     (description
